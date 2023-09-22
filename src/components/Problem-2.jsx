c70bb9d1f010e5b92a7a73fb14e87834b37a1dc2
Tanvir Ahmad <tanvirahmadstudent@gmail.com>
2023-09-22 22:48:49 +0600

    implement modal

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,19 +1,49 @@
-import React from 'react';
+import ReactModal from 'react-modal';
+
+import { useEffect, useState } from "react";
+
 
 const Problem2 = () => {
+    const [contacts, setContacts] = useState([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    useEffect(() => {
+        fetch('https://contact.mediusware.com/api/contacts/')
+            .then(res => res.json())
+            .then(data => setContacts(data.results))
+    }, [])
+    const usContacts = contacts.filter(item=>item=='usa');
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
 
     return (
 
         <div className="container">
             <div className="row justify-content-center mt-5">
                 <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
+
                 <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
+                    <button className="btn btn-lg btn-outline-primary" onClick={openModal} type="button" >All Contacts</button>
+                    <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
                 </div>
-                
+
             </div>
+            <ReactModal
+                isOpen={isModalOpen}
+                onRequestClose={closeModal}
+                contentLabel="All Contacts Modal"
+            >
+                <h2>All Contacts</h2>
+                {
+
+                }
+                <button onClick={closeModal}>Close Modal</button>
+            </ReactModal>
+
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import '../src/assets/scss/App.scss';
 import {BrowserRouter} from "react-router-dom";
+import Modal from 'react-modal';
 
+// Set the App element
+Modal.setAppElement('#root'); 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <BrowserRouter>
