3f804a46928e2a45907528c7535c75a67aab1736
Tanvir Ahmad <tanvirahmadstudent@gmail.com>
2023-09-23 08:08:17 +0600

    implement problem-2

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -5,21 +5,31 @@ import { useEffect, useState } from "react";
 
 const Problem2 = () => {
     const [contacts, setContacts] = useState([]);
+    const [show, setShow] = useState('all');
     const [isModalOpen, setIsModalOpen] = useState(false);
     useEffect(() => {
         fetch('https://contact.mediusware.com/api/contacts/')
             .then(res => res.json())
             .then(data => setContacts(data.results))
     }, [])
-    const usContacts = contacts.filter(item=>item=='usa');
+
     const openModal = () => {
         setIsModalOpen(true);
     };
     const closeModal = () => {
         setIsModalOpen(false);
     };
-
-
+    console.log(contacts);
+    let showContacts = [];
+    if (show === 'usa') {
+        showContacts = contacts.filter(item => item.country.name == 'United States');
+    } else {
+        showContacts = [...contacts];
+    }
+    const handleContacts = (countryName) => {
+        openModal();
+        setShow(countryName)
+    }
     return (
 
         <div className="container">
@@ -27,8 +37,8 @@ const Problem2 = () => {
                 <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
 
                 <div className="d-flex justify-content-center gap-3">
-                    <button className="btn btn-lg btn-outline-primary" onClick={openModal} type="button" >All Contacts</button>
-                    <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
+                    <button className="btn btn-lg btn-outline-primary" onClick={() => handleContacts('all')} type="button" >All Contacts</button>
+                    <button className="btn btn-lg btn-outline-warning" onClick={() => handleContacts('usa')} type="button" >US Contacts</button>
                 </div>
 
             </div>
@@ -37,11 +47,27 @@ const Problem2 = () => {
                 onRequestClose={closeModal}
                 contentLabel="All Contacts Modal"
             >
-                <h2>All Contacts</h2>
-                {
+                <div className="d-flex justify-content-center gap-3">
+                    <button className={`btn btn-lg ${show == 'all' ? 'btn-primary' : 'btn-outline-primary'} `} onClick={() => handleContacts('all')} type="button" >All Contacts</button>
+                    <button className={`btn btn-lg ${show == 'usa' ? 'btn-warning' : 'btn-outline-warning'} `} onClick={() => handleContacts('usa')} type="button" >US Contacts</button>
+                    <button onClick={closeModal} className={`btn btn-lg btn-outline-danger `}>Close Modal</button>
+                </div>
+                <div className='container'>
+                    <div className='row row-cols-2'>
+                        {showContacts.map(item => (
+                            <div className='col my-2 ' key={item.id}>
+                                <div className='card p-2'>
+                                    <p><strong>Country Name: </strong>{item.country.name}</p>
+                                    <p><strong>Phone Number:</strong> {item.phone}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
 
-                }
-                <button onClick={closeModal}>Close Modal</button>
+
+
+                
             </ReactModal>
 
         </div>
